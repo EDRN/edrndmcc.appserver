e4e0a400d621431da08cfd19b0390c0191bd8260
Sean Kelly <sean.c.kelly@me.com>
2012-09-23 21:27:00 +0000

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import os.path
 # ------------
 
 _name            = 'edrndmcc.appserver'
-_version         = '1.2.0'
+_version         = '1.2.1'
 _description     = 'EDRN DMCC Application Server policy'
 _url             = 'http://cancer.jpl.nasa.gov/'
 _downloadURL     = 'http://oodt.jpl.nasa.gov/dist/edrndmcc-appserver'
